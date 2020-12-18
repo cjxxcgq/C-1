39ab3939a369eab04cfb789510c7266c207d6eff
tylgdxcgq <tylgdxcgq@hotmail.com>
2020-12-18 13:04:57 +0800

    o201208

--- C++1/C++1.cpp
+++ C++1/C++1.cpp
@@ -33,13 +33,35 @@ protected:
 private:
 };
 
+void Lambda()
+{
+	int m = [](int x) { return [](int y) { return y * 2; }(x)+6; }(5);                     //输入(5);
+	std::cout << "m:" << m << std::endl;                                                  //输出m:16
+	std::cout << "n:" << [](int x, int y) { return x + y; }(5, 4) << std::endl;            //输出n:9
 
+	auto gFunc = [](int x) -> function<int(int)> { return [=](int y) { return x + y; }; };
+	auto lFunc = gFunc(4);        std::cout << lFunc(5) << std::endl;
+	auto hFunc = [](const function<int(int)>& f, int z) { return f(z) + 1; };
+	auto a = hFunc(gFunc(7), 8);
+	int a = 111, b = 222;
+	auto func = [=, &b]()mutable { a = 22; b = 333; std::cout << "a:" << a << " b:" << b << std::endl; };
+	func();
+	std::cout << "a:" << a << " b:" << b << std::endl;
+	a = 333;
+	auto func2 = [=, &a] { a = 444; std::cout << "a:" << a << " b:" << b << std::endl; };
+	func2();
+	auto func3 = [](int x) ->function<int(int)> { return [=](int y) { return x + y; }; };
+
+	std::function<void(int x)> f_display_42  = [](int x) { print_num(x); };
+	f_display_42(44);
+
+}
 int main()
 {
 	Point Pt_a(1.f,5.f), Pt_y;
 	Pt_y.X() = 3;
 	Pt_y.Y() = 3;
-
+	Lambda();
 
 	ATLTRACE("Output Pt: (%7.2f,%7.2f)\n",Pt_a.X(), Pt_a.Y());
 	ATLTRACE("Output -Pt: (%7.2f,%7.2f)\n",(-Pt_a).X(),(-Pt_a).Y());
